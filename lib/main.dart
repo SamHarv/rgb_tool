700bff14090cfdea306941720bc8a84fac7d4b40
Sam Harvey <srharvey93@gmail.com>
2025-02-13 14:27:57 +1100

    Added hex value

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,7 @@ class Colours extends StatelessWidget {
   const Colours({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RGB Colours Tool',
-      home: const ColourPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(home: const ColourPage());
 }
 
 class ColourPage extends StatefulWidget {
@@ -23,6 +18,8 @@ class ColourPage extends StatefulWidget {
 
 class _ColourPageState extends State<ColourPage> {
   int r = 255, g = 255, b = 255;
+  String rgb2hex(int r, int g, int b) =>
+      '#${r.toRadixString(16).padLeft(2, '0')}${g.toRadixString(16).padLeft(2, '0')}${b.toRadixString(16).padLeft(2, '0')}';
 
   @override
   Widget build(BuildContext context) {
@@ -34,58 +31,59 @@ class _ColourPageState extends State<ColourPage> {
       backgroundColor: Color.fromARGB(255, r, g, b),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title: Text(
-          'RGB Colour Tool',
-          style: TextStyle(color: invertedColour),
-        ),
+        title: Text('RGB Colour Tool',
+            style: TextStyle(color: invertedColour, fontSize: 32)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              children: [
-                Text(
-                  'Red: $r',
-                  style: TextStyle(color: invertedColour, fontSize: 24),
-                ),
-                Slider(
-                  activeColor: invertedColour,
-                  value: r.toDouble(),
-                  min: 0,
-                  max: 255,
-                  onChanged: (value) => setState(() => r = value.toInt()),
-                ),
-              ],
+            Text(
+              'Hex: ${rgb2hex(r, g, b).toUpperCase()}',
+              style: TextStyle(color: invertedColour, fontSize: 24),
             ),
             Column(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text(
-                  'Green: $g',
-                  style: TextStyle(color: invertedColour, fontSize: 24),
+                Column(
+                  children: [
+                    Text('Red: $r',
+                        style: TextStyle(color: invertedColour, fontSize: 24)),
+                    Slider(
+                      activeColor: invertedColour,
+                      value: r.toDouble(),
+                      min: 0,
+                      max: 255,
+                      onChanged: (value) => setState(() => r = value.toInt()),
+                    ),
+                  ],
                 ),
-                Slider(
-                  activeColor: invertedColour,
-                  value: g.toDouble(),
-                  min: 0,
-                  max: 255,
-                  onChanged: (value) => setState(() => g = value.toInt()),
+                Column(
+                  children: [
+                    Text('Green: $g',
+                        style: TextStyle(color: invertedColour, fontSize: 24)),
+                    Slider(
+                      activeColor: invertedColour,
+                      value: g.toDouble(),
+                      min: 0,
+                      max: 255,
+                      onChanged: (value) => setState(() => g = value.toInt()),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Column(
-              children: [
-                Text(
-                  'Blue: $b',
-                  style: TextStyle(color: invertedColour, fontSize: 24),
-                ),
-                Slider(
-                  activeColor: invertedColour,
-                  value: b.toDouble(),
-                  min: 0,
-                  max: 255,
-                  onChanged: (value) => setState(() => b = value.toInt()),
+                Column(
+                  children: [
+                    Text('Blue: $b',
+                        style: TextStyle(color: invertedColour, fontSize: 24)),
+                    Slider(
+                      activeColor: invertedColour,
+                      value: b.toDouble(),
+                      min: 0,
+                      max: 255,
+                      onChanged: (value) => setState(() => b = value.toInt()),
+                    ),
+                  ],
                 ),
               ],
             ),
